4784f5fccdaada79dd6966c92daf57e62d9d2258
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2020-04-29 00:57:07 -0230

    Add a basic SCS template

--- R/utils.R
+++ R/utils.R
@@ -2,12 +2,12 @@
 #' Creates an R Markdown Word NAFO-formatted document
 #'
 #' This is a function called in output in the YAML of the driver Rmd file
-#' to specify the creation of a Microsoft Word version of an SCR or a
-#' STACFIC report.
+#' to specify the creation of a Microsoft Word version of an SCR, STACFIC
+#' or SCS report.
 #'
 #' @param ... arguments to pass to [bookdown::word_document2()]
 #'
-#' @return A Word Document based on the NAFO SCR word template.
+#' @return A Word Document based on the NAFO SCR, STACFIC or SCS word template.
 #'
 #' @import bookdown
 #' @rdname word_scr
@@ -35,6 +35,22 @@ word_stacfis <- function(...) {
 
     base <- word_document2(...,
                            reference_docx = system.file("docx", "STACFIS_template.docx", package = "NAFOdown")
+    )
+
+    # Mostly copied from knitr::render_sweave
+    base$knitr$opts_chunk$comment <- NA
+    base$knitr$opts_chunk$fig.align <- "center"
+    base
+
+}
+
+#' @rdname word_scr
+#' @export
+#'
+word_scs <- function(...) {
+
+    base <- word_document2(...,
+                           reference_docx = system.file("docx", "SCS_template.docx", package = "NAFOdown")
     )
 
     # Mostly copied from knitr::render_sweave
