fde1a45bdbce1ace1979fdb93ef0c31917d99ca9
Bob Rogers <rpwr00@mun.ca>
2020-05-25 13:41:13 -0230

    Modified the line and rect parameters in theme_nafo

--- R/figures.R
+++ R/figures.R
@@ -22,8 +22,8 @@
 
 theme_nafo <- function(base_size = 14, base_family = "serif"){
     theme_foundation(base_size = base_size, base_family = base_family) %+replace%
-        theme(rect = element_rect(color = "black", size = 0.3),
-              line = element_line(colour = "black", size = 0.3),
+        theme(rect = element_rect(color = "black", size = 0.3,linetype="solid",fill=NA),
+              line = element_line(colour = "black", size = 0.3,linetype="solid",lineend="butt"),
               panel.grid.minor = element_blank(),
               panel.grid.major = element_blank(),
               plot.background = element_blank(),
