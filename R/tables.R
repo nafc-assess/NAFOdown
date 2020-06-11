759605499600c35ca7f2125de1468af137def8d4
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2020-06-10 23:21:18 -0230

    Add legend to summary table and parse option

--- R/tables.R
+++ R/tables.R
@@ -86,7 +86,8 @@ theme_nafotabs <- function(x, fontsize = 11, fontname = "Cambria") {
     p$vertical.align[as.logical(txt_as_sub)] <- "subscript"
     p$vertical.align[as.logical(txt_as_sup)] <- "superscript"
     p <- p[!(p$txt %in% c("*", "__", "~", "^")), ] # drop special characters used to define formatting
-    p$txt <- paste0(p$txt, " ")
+    p$txt[-1] <- ifelse(p$vertical.align[-1] %in% c("subscript", "superscript"),
+                        p$txt[-1], paste0(" ", p$txt[-1]))
     p$seq_index <- seq(nrow(p))
     para[[1]] <- p
     para
@@ -100,12 +101,14 @@ theme_nafotabs <- function(x, fontsize = 11, fontname = "Cambria") {
 #'
 #' @param comments  A vector of length 5 elaborating on the status the status of each 'Convention General Principle'
 #' @param status    A vector of length 5 decribing the status of each 'Convention General Principle'. Must be one of: 'OK', 'Intermediate','Not accomplished', 'Unknown'
+#' @param parse     Parse comments with markdown like syntax (experimental!)
 #'
 #' @export
 #'
 #'
 
-nafo_summary_table <- function(comments = rep(NA, 5), status = rep(NA, 5)) {
+nafo_summary_table <- function(comments = rep(NA, 5), status = rep(NA, 5),
+                               parse = FALSE) {
 
     if (length(comments) != 5) {
         stop("Comments must be a vector of length = 5 (for each row in table)")
@@ -125,18 +128,22 @@ nafo_summary_table <- function(comments = rep(NA, 5), status = rep(NA, 5)) {
             "Preserve marine biodiversity"
         ),
         "Status" = status,
-        "Comment" = comments
+        "Comment" = comments,
+        "a" = " ",
+        "b" = " "
     )
-    names(stock_summary_df) <- c("Convention General Principles", "Status", "Comment/consideration")
+    names(stock_summary_df) <- c("Convention General Principles", "Status",
+                                 "Comment/consideration", " ", "  ")
 
+    ## TODO: figure out how to export images to a word table rather than unicode symbols
     green_light <- system.file("graphics", "green-light.jpg", package = "NAFOdown")
     yellow_light <- system.file("graphics", "yellow-light.jpg", package = "NAFOdown")
     red_light <- system.file("graphics", "red-light.jpg", package = "NAFOdown")
     white_light <- system.file("graphics", "white-light.jpg", package = "NAFOdown")
 
     summary_table <- flextable(stock_summary_df) %>%
-        theme_nafotabs() %>%
-        width(width = c(2.9, 0.65, 2.9)) %>%
+        theme_nafotabs(fontsize = 10) %>%
+        width(width = c(2.2, 0.6, 2.2, 0.5, 1)) %>%
         align(align = "left", part = "all") %>%
         compose(j = 1, i = 1, part = "body",
                 value = as_paragraph("Restore or maintain at ", as_i("B"), as_i(as_sub("MSY")))) %>%
@@ -145,8 +152,24 @@ nafo_summary_table <- function(comments = rep(NA, 5), status = rep(NA, 5)) {
         color(j = 2, i = ~Status == "Intermediate", color = "#EFB700") %>%
         color(j = 2, i = ~Status == "Not accomplished", color = "#B81D13") %>%
         color(j = 2, i = ~Status == "Unknown", color = "#a6a6a6") %>%
-        fontsize(j = 2, size = 15) %>%
+        fontsize(j = 2, size = 12) %>%
+        align(j = 2, align = "center") %>%
+        compose(j = 4, i = 1:4, value = as_paragraph("\U25CF")) %>%
+        color(j = 4, i = 1:4, color = c("#008450", "#EFB700", "#B81D13", "#a6a6a6")) %>%
+        compose(j = 5, i = 1:4, value = as_paragraph(c("OK", "Intermediate",
+                                                       "Not accomplished", "Unknown"))) %>%
+        align(j = 4, align = "right") %>%
+        border(j = 4:5, part = "all", border = officer::fp_border(width = 0)) %>%
         italic(i = 1, part = "header")
+
+    if (parse) {
+        summary_table <- summary_table %>%
+            compose(i = 1, j = 3, value = .parse(stock_summary_df[1, 3])) %>%
+            compose(i = 2, j = 3, value = .parse(stock_summary_df[2, 3])) %>%
+            compose(i = 3, j = 3, value = .parse(stock_summary_df[3, 3])) %>%
+            compose(i = 4, j = 3, value = .parse(stock_summary_df[4, 3])) %>%
+            compose(i = 5, j = 3, value = .parse(stock_summary_df[5, 3]))
+    }
 
     summary_table
 
