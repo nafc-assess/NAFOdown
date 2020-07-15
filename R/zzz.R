41c6b8d13767f0720922c60ce3ac92cdb2e52dcf
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2020-07-15 15:52:58 -0230

    During .onLoad, run a check for the Cambria font before running font_add

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,9 @@
 #' @import showtext
 
 .onLoad <- function(libname, pkgname){
-    sysfonts::font_add("Cambria", "cambria.ttc") # font used for NAFO plots
+    fonts <- list.files(sysfonts::font_paths())
+    if ("cambria.ttc" %in% fonts) {
+        sysfonts::font_add("Cambria", "cambria.ttc") # font used for NAFO plots
+    }
     showtext::showtext_auto()
 }
