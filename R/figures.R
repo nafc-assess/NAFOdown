f8e0f61a52736497c175c95782d85b681f47c039
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-06-13 11:28:11 -0300

    Add function for producing ridge plots from annual length frequency data

--- R/figures.R
+++ R/figures.R
@@ -88,3 +88,78 @@ NULL
 #' @rdname nafo-vals
 .nafo_asp <- 6.4 / 11.5
 
+
+
+
+#' Generate ridges plot from annual length frequency data
+#'
+#' @description Function for producing a faceted ridge plot of annual length frequencies using the ggridges package.
+#'
+#' @param data           A data.frame with `"year"`, `"length"`, and `"frequency"` headers. Extra columns
+#'                       may aso be utilized to define facets using the rows and cols arguments.
+#' @param rows           Column to use to define facet rows.
+#' @param cols           Column to use to define facet columns.
+#' @param xlab           Label for x-axis.
+#' @param ylab           Label for y-axis.
+#' @param scale          Scaling factor controlling the height of the ridges.
+#' @param alpha          Value controling the transparancy of the ridges.
+#' @param scale_color    Function such as [ggplot2::scale_colour_viridis_d] for defining line
+#'                       colours to use across years.
+#' @param scale_fill     Function such as [ggplot2::scale_fill_viridis_d] for defining fill
+#'                       colours to use across years.
+#' @param drop_zeros     Should zeros be replaced by NA values? (i.e., impose blank space in ridge plot).
+#'
+#' @return Returns a ggplot2 object that can be modified further by functions such as [ggplot2::theme()].
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ## Simulate some data
+#' set.seed(3)
+#' samples_per_year <- 1000
+#' years <- 2000:2022
+#' lambdas <- rnorm(length(years), mean = 25, sd = 10)
+#' data <- lapply(seq_along(years), function(i) {
+#'     lengths <- rpois(samples_per_year, lambda = lambdas[i])
+#'     lf <- table(lengths)
+#'     data.frame(year = years[i], length = as.numeric(names(lf)), frequency = as.numeric(unname(lf)))
+#' })
+#' data <- do.call(rbind, data)
+#'
+#' ## Using defaults
+#' plot_lengths(data)
+#'
+#' ## Customize using ggplot functions
+#' plot_lengths(data, alpha = 0.1, scale = 2) +
+#' scale_color_manual(values = rep("grey30", length(unique(data$year)))) +
+#' scale_fill_manual(values = rep("grey30", length(unique(data$year))))
+#'
+#'
+#' @import ggridges
+#'
+plot_lengths <- function(data, rows = NULL, cols = NULL,
+                         xlab = "Length (cm)", ylab = "Year",
+                         scale = 5, alpha = 0.8,
+                         scale_color = scale_color_viridis_d,
+                         scale_fill = scale_fill_viridis_d,
+                         drop_zeros = TRUE) {
+
+    if (drop_zeros) data$freq[data$freq == 0] <- NA
+
+    ggplot(data)  +
+        theme_ridges(grid = TRUE, center_axis_labels = TRUE, font_size = 3.5) +
+        geom_density_ridges(aes(x = as.numeric(length), y = as.factor(year),
+                                height = as.numeric(frequency), fill = as.factor(year),
+                                color = as.factor(year)),
+                            stat = "identity", scale = scale, alpha = alpha, show.legend = FALSE) +
+        facet_grid(rows = rows, cols = cols, scales = "free_y", space = "free_y") +
+        scale_color() + scale_fill() +
+        labs(x = xlab, y = ylab)+
+        theme_nafo()+
+        theme(panel.background = element_blank(),
+              panel.grid = element_blank(),
+              panel.spacing.x = unit(0,"line"))
+
+}
+
