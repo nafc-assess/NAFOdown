a30d64eaa46705a76fa7d6c7e7c54f8ec7c4714d
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-06 17:04:02 -0300

    Enable the use of officedown to allow more customization of Word documents

--- NEWS.md
+++ NEWS.md
@@ -4,3 +4,4 @@
 - Adapted csasdown for NAFO documents
 - Added a ggplot and flextable themes for producing plots and tables that comply with NAFO plotting guidelines
 - Added a function for producing ridge plots from annual length frequency data
+- Enable the use of bookdown::word_document2 or officedown::rdocx_document to build Word documents (see ?word_scr)
--- R/utils.R
+++ R/utils.R
@@ -15,25 +15,37 @@
 #'
 #' This is a function called in output in the YAML of the driver Rmd file
 #' to specify the creation of a Microsoft Word version of an SCR, STACFIC
-#' or SCS report.
+#' or SCS report. Functions with 2 as a suffix are built using
+#' [officedown::rdocx_document()], allowing users to leverage more in depth
+#' customization of Word output.
 #'
-#' @param ... arguments to pass to [bookdown::word_document2()]
+#' @param ... arguments to pass to [bookdown::word_document2()] or [officedown::rdocx_document()]
+#'            for functions with 2 as a suffix.
 #'
 #' @return A Word Document based on the NAFO SCR, STACFIC or SCS word template.
 #'
 #' @import bookdown
+#' @import officedown
 #' @rdname word_scr
 #' @export
-#'
 word_scr <- .make_word_function(bookdown::word_document2, "SCR_template.docx")
 
 #' @rdname word_scr
 #' @export
-#'
+word_scr2 <- .make_word_function(officedown::rdocx_document, "SCR_template.docx")
+
+#' @rdname word_scr
+#' @export
 word_stacfis <- .make_word_function(bookdown::word_document2, "STACFIS_template.docx")
 
 #' @rdname word_scr
 #' @export
-#'
+word_stacfis2 <- .make_word_function(officedown::rdocx_document, "STACFIS_template.docx")
+
+#' @rdname word_scr
+#' @export
 word_scs <- .make_word_function(bookdown::word_document2, "SCS_template.docx")
 
+#' @rdname word_scr
+#' @export
+word_scs2 <- .make_word_function(officedown::rdocx_document, "SCS_template.docx")
