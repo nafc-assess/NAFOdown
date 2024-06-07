58f84a6e3c105ee6deb5da6e99ae283c00162396
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-07 18:25:41 -0300

    Specify dpi for showtext on load to avoid small labels for exported plots when users are using NAFOdown

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 
-
 #' @importFrom  sysfonts font_add
 #' @import showtext
 
-.onLoad <- function(libname, pkgname){
+.onLoad <- function(libname, pkgname) {
     fonts <- list.files(sysfonts::font_paths())
     if ("cambria.ttc" %in% fonts) {
         sysfonts::font_add("Cambria", "cambria.ttc") # font used for NAFO plots
     }
     showtext::showtext_auto()
+    showtext::showtext_opts(dpi = 300)
 }
