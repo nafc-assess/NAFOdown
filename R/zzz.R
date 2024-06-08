f79459bd20d491f9d364e3acc44c616104c8041a
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-07 22:06:03 -0300

    Add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -10,3 +10,18 @@
     showtext::showtext_auto()
     showtext::showtext_opts(dpi = 300)
 }
+
+
+.onAttach <- function(libname, pkgname) {
+    ## rfiglet startup message
+    packageStartupMessage(
+        "    _   _____    __________      __\n",
+        "   / | / /   |  / ____/ __ \\____/ /___ _      ______\n",
+        "  /  |/ / /| | / /_  / / / / __  / __ \\ | /| / / __ \\\n",
+        " / /|  / ___ |/ __/ / /_/ / /_/ / /_/ / |/ |/ / / / /\n",
+        "/_/ |_/_/  |_/_/    \\____/\\__,_/\\____/|__/|__/_/ /_/",
+        " version ", packageVersion(pkgname), "\n\n",
+        "Please report issues to https://github.com/nafc-assess/NAFOdown/issues, and\n",
+        "send references to documents using NAFOdown to Paul.Regular@dfo-mpo.gc.ca."
+        )
+}
