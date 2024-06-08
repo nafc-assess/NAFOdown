07e2397c6d00fcb48b09179b05131612f5a2d27d
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-07 22:20:49 -0300

    tweak to try and pass CMD check

--- R/zzz.R
+++ R/zzz.R
@@ -20,7 +20,7 @@
         "  /  |/ / /| | / /_  / / / / __  / __ \\ | /| / / __ \\\n",
         " / /|  / ___ |/ __/ / /_/ / /_/ / /_/ / |/ |/ / / / /\n",
         "/_/ |_/_/  |_/_/    \\____/\\__,_/\\____/|__/|__/_/ /_/",
-        " version ", packageVersion(pkgname), "\n\n",
+        " version ", utils::packageVersion(pkgname), "\n\n",
         "Please report issues to https://github.com/nafc-assess/NAFOdown/issues, and\n",
         "send references to documents using NAFOdown to Paul.Regular@dfo-mpo.gc.ca."
         )
