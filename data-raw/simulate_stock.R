09c614427e66d93c700977a82a7b54caa84d1cb1
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-12 17:10:55 -0300

    Export toy stock for excel plotting

--- data-raw/simulate_stock.R
+++ data-raw/simulate_stock.R
@@ -85,32 +85,34 @@ theme_set(theme_nafo()) # set default ggplot2 theme
 C_plot <- ggplot(aes(x = year), data = toy_stock) +
     geom_bar(aes(y = catch, fill = "Catch"), stat = "identity") +
     geom_line(aes(y = TAC, linetype = "TAC")) +
-    scale_y_continuous(n.breaks = 10, name = "Catch / TAC") +
-    scale_x_continuous(n.breaks = 10, name = "") +
+    scale_y_continuous(n.breaks = 10, name = "Catch / TAC", expand = expansion(c(0, 0.05))) +
+    scale_x_continuous(n.breaks = 10, name = "", expand = expansion(c(0, 0))) +
     scale_fill_nafo() +
     theme(legend.title = element_blank())
 
 B_plot <- ggplot(aes(x = year), data = toy_stock) +
     geom_ribbon(aes(ymin = relB_lwr, ymax = relB_upr), fill = .nafo_cols[1], alpha = 0.4) +
     geom_line(aes(y = relB), color = .nafo_cols[1]) +
-    scale_y_continuous(limits = c(0, NA), n.breaks = 10, name = "B/Blim") +
-    scale_x_continuous(n.breaks = 10, name = "")
+    scale_y_continuous(limits = c(0, NA), n.breaks = 10, name = "B/Blim", expand = expansion(c(0, 0.05))) +
+    scale_x_continuous(n.breaks = 10, name = "", expand = expansion(c(0, 0)))
 
 F_plot <- ggplot(aes(x = year), data = toy_stock) +
     geom_ribbon(aes(ymin = relF_lwr, ymax = relF_upr), fill = .nafo_cols[1], alpha = 0.4) +
     geom_line(aes(y = relF), color = .nafo_cols[1]) +
-    scale_y_continuous(limits = c(0, NA), n.breaks = 10, name = "F/Flim") +
-    scale_x_continuous(n.breaks = 10, name = "")
+    scale_y_continuous(limits = c(0, NA), n.breaks = 10, name = "F/Flim", expand = expansion(c(0, 0.05))) +
+    scale_x_continuous(n.breaks = 10, name = "", expand = expansion(c(0, 0)))
 
 R_plot <- ggplot(aes(x = year), data = toy_stock) +
     geom_line(aes(y = rec_index1, color = "Survey 1")) +
     geom_line(aes(y = rec_index2, color = "Survey 2")) +
     geom_line(aes(y = rec_index3, color = "Survey 3")) +
-    scale_y_continuous(limits = c(0, NA), name = "Recruitment index") +
-    scale_x_continuous(n.breaks = 10, name = "") +
+    scale_y_continuous(limits = c(0, NA), name = "Recruitment index", expand = expansion(c(0, 0.05))) +
+    scale_x_continuous(n.breaks = 10, name = "", expand = expansion(c(0, 0))) +
     scale_color_nafo() +
     theme(legend.title = element_blank())
 
 (C_plot + B_plot) / (F_plot + R_plot)
 
+
+write.csv(toy_stock, file = "data-raw/toy_stock.csv", row.names = FALSE)
 usethis::use_data(toy_stock, overwrite = TRUE)
