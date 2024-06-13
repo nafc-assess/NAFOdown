1c3c6f85ec945f731c5975c8941d45edc194cbef
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-06-13 19:11:30 -0300

    Tweaks following checks of png export

--- inst/examples/scale_colour_nafo.R
+++ inst/examples/scale_colour_nafo.R
@@ -15,22 +15,30 @@ sel_curves <- lapply(seq_along(years), function(y) {
 })
 sel_curves <- do.call(rbind, sel_curves)
 
-ggplot(sel_curves, aes(x = age, y = sel, colour = as.factor(year))) +
-    geom_line(linewidth = 1) +
+sel_plot <- ggplot(sel_curves, aes(x = age, y = sel, colour = as.factor(year))) +
+    geom_line() +
     scale_y_continuous(name = "Selectivity", expand = expansion(c(0.01, 0.01))) +
     scale_x_continuous(name = "Age", expand = expansion(c(0.01, 0.01))) +
     scale_colour_nafo(palette = "blue2green", name = "Year") +
     theme(legend.position = "right",
           legend.box.background = element_blank())
+sel_plot
 
 res <- expand.grid(age = 1:10, year = 1990:2020)
 res$res <- rnorm(nrow(res))
-ggplot(res, aes(x = year, y = age, size = res, fill = res)) +
-    geom_point(shape = 21, colour = "#f0f0f0") +
-    scale_size_area(max_size = 10, name = "Residual") +
+res_plot <- ggplot(res, aes(x = year, y = age, size = res, fill = res)) +
+    geom_point(shape = 21, colour = "#f0f0f0", stroke = .nafo_lwd) +
+    scale_size_area(max_size = 8, name = "Residual") +
     scale_fill_nafo(type = "continuous", palette = "hot_cold", name = "Residual") +
-    scale_y_continuous(name = "Age") +
+    scale_y_continuous(name = "Age", n.breaks = 10) +
     scale_x_continuous(name = "Year", n.breaks = 10) +
     theme(legend.position = "right",
           legend.box.background = element_blank())
+res_plot
+
+if (interactive()) {
+    showtext::showtext_opts(dpi = 300)
+    ggsave("selectivity_plot.png", plot = sel_plot, height = .nafo_height, width = .nafo_width)
+    ggsave("residual_plot.png", plot = res_plot, height = .nafo_height, width = .nafo_width)
+}
 
