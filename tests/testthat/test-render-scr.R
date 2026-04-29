e1b20bf59f110cc71654614fa252ced84a8bdffa
Paul Regular <PaulRegular@users.noreply.github.com>
2026-04-29 15:54:52 -0230

    Fix DESCRIPTION format and use draft() in render test

--- tests/testthat/test-render-scr.R
+++ tests/testthat/test-render-scr.R
@@ -2,21 +2,18 @@ test_that("default SCR template renders", {
   skip_if_not_installed("rmarkdown")
   skip_if_not_installed("bookdown")
 
-  skeleton <- system.file(
-    "rmarkdown/templates/SCR/skeleton/skeleton.Rmd",
-    package = "NAFOdown"
-  )
-  expect_true(file.exists(skeleton))
-
   workdir <- tempfile("nafodown-scr-")
   dir.create(workdir)
 
-  source_dir <- dirname(skeleton)
-  file.copy(source_dir, workdir, recursive = TRUE)
-  render_dir <- file.path(workdir, basename(source_dir))
+  old_wd <- getwd()
+  setwd(workdir)
+  on.exit(setwd(old_wd), add = TRUE)
+
+  NAFOdown::draft("SCR", create_dir = FALSE, edit = FALSE)
+  expect_true(file.exists(file.path(workdir, "index.Rmd")))
 
   output <- rmarkdown::render(
-    input = file.path(render_dir, "skeleton.Rmd"),
+    input = file.path(workdir, "index.Rmd"),
     output_format = "NAFOdown::word_scr",
     quiet = TRUE,
     envir = new.env(parent = globalenv())
