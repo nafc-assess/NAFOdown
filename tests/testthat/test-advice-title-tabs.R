9588de81ffc912c81fc96fe744d45c8546c0cfa5
Paul Regular <PaulRegular@users.noreply.github.com>
2026-06-01 17:53:02 -0300

    Inject stock advice title tabs during render

---
+++ tests/testthat/test-advice-title-tabs.R
@@ -0,0 +1,45 @@
+test_that("advice title tabs are injected in title custom styles", {
+  lines <- c(
+    '::: {custom-style="Title"}',
+    "",
+    "__Species X in Divs. Y__ Advice 2026 for 2027",
+    "",
+    ":::",
+    "Body Advice text stays unchanged"
+  )
+
+  updated <- NAFOdown:::.inject_advice_title_tabs(lines)
+
+  expect_identical(
+    updated[3],
+    "__Species X in Divs. Y__`<w:r><w:tab/></w:r>`{=openxml}Advice 2026 for 2027"
+  )
+  expect_identical(updated[6], lines[6])
+})
+
+test_that("advice title tabs are not duplicated", {
+  lines <- c(
+    '::: {custom-style="Title"}',
+    "__Species X in Divs. Y__`<w:r><w:tab/></w:r>`{=openxml}Advice 2026 for 2027",
+    ":::"
+  )
+
+  updated <- NAFOdown:::.inject_advice_title_tabs(lines)
+
+  expect_identical(updated, lines)
+})
+
+test_that("advice title tab injection updates markdown files", {
+  input_file <- tempfile(fileext = ".md")
+  writeLines(c(
+    '::: {custom-style="Title"}',
+    "__Species X in Divs. Y__ Advice 2026 for 2027",
+    ":::"
+  ), input_file)
+
+  changed <- NAFOdown:::.inject_advice_title_tabs_file(input_file)
+
+  expect_true(changed)
+  expect_true(grepl("<w:tab/>", readLines(input_file, warn = FALSE)[2],
+                    fixed = TRUE))
+})
